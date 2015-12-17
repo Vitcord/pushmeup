cdd9cd638b0ea7b3bab4cf017552b8b3551243b7
Dion Durigon <dion@conx2share.com>
2015-12-17 09:05:36 -0500

    Updated Ruby version and included category in Apple Push Notification package

--- lib/pushmeup/apns/notification.rb
+++ lib/pushmeup/apns/notification.rb
@@ -1,11 +1,12 @@
 module APNS
   class Notification
-    attr_accessor :device_token, :alert, :badge, :sound, :other
+    attr_accessor :device_token, :alert, :category, :badge, :sound, :other
 
     def initialize(device_token, message)
       self.device_token = device_token
       if message.is_a?(Hash)
         self.alert = message[:alert]
+        self.category = message[:category]
         self.badge = message[:badge]
         self.sound = message[:sound]
         self.other = message[:other]
@@ -29,6 +30,7 @@ module APNS
     def packaged_message
       aps = {'aps'=> {} }
       aps['aps']['alert'] = self.alert if self.alert
+      aps['aps']['category'] = self.category if self.category
       aps['aps']['badge'] = self.badge if self.badge
       aps['aps']['sound'] = self.sound if self.sound
       aps.merge!(self.other) if self.other
@@ -38,6 +40,7 @@ module APNS
     def ==(that)
       device_token == that.device_token &&
       alert == that.alert &&
+      category == that.category &&
       badge == that.badge &&
       sound == that.sound &&
       other == that.other
